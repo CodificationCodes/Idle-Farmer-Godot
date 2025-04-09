948014a30ff21b0fbf9533d6f10a2003145bf3e8
Codi <144501563+CodificationCodes@users.noreply.github.com>
2025-04-09 13:01:41 +1000

    First grow state of Beetroots

--- Scripts.gd/world.gd
+++ Scripts.gd/world.gd
@@ -3,16 +3,13 @@ extends Node
 @onready var tile_map: TileMap = $TileMap
 
 func _process(delta):
+	var mouse_pos: Vector2 = tile_map.get_global_mouse_position()
+	var local_mouse_pos: Vector2 = tile_map.to_local(mouse_pos)
+	var tile_map_pos: Vector2i = tile_map.local_to_map(local_mouse_pos)
+	var tileSource = tile_map.get_cell_source_id(0, tile_map_pos)
 	if Input.is_action_just_pressed("interact"):
 		print("Right-click detected")
 
-		# get mouse pos/water dirt
-		var mouse_pos: Vector2 = tile_map.get_global_mouse_position()
-
-		var local_mouse_pos: Vector2 = tile_map.to_local(mouse_pos)
-
-		var tile_map_pos: Vector2i = tile_map.local_to_map(local_mouse_pos)
-		var tileSource = tile_map.get_cell_source_id(0, tile_map_pos)
 		print(tileSource)
 
 		if tileSource == 2 and tile_map.get_cell_atlas_coords(0, tile_map_pos) == Vector2i(1, 0):
@@ -28,4 +25,8 @@ func _process(delta):
 
 	if Input.is_action_just_pressed("plant"):
 		if tileSource == 3 and tile_map.get_cell_atlas_coords(0, tile_map_pos) == Vector2i(1,0):
-			tile_map.set_cell(0, tile_map_pos, 5, Vector2i(0,0), 0)
+			tile_map.set_cell(0, tile_map_pos, 4, Vector2i(1,0), 0)
+		elif tileSource == 3 and tile_map.get_cell_atlas_coords(0,tile_map_pos) == Vector2i(0,0):
+			tile_map.set_cell(0, tile_map_pos, 4, Vector2i(0,0), 0)
+		elif tileSource == 3 and tile_map.get_cell_atlas_coords(0,tile_map_pos) == Vector2i(2,0):
+			tile_map.set_cell(0, tile_map_pos, 4, Vector2i(2,0), 0)
