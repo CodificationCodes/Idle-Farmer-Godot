35a4907d77e9487adc6aa49e299cc2cd358460d3
Codi <144501563+CodificationCodes@users.noreply.github.com>
2025-05-02 10:27:39 +1000

    Full Beetroot Implementation
    Updated and added the base function for all fruits and growable vegetables.

--- Scripts.gd/world.gd
+++ Scripts.gd/world.gd
@@ -22,11 +22,19 @@ func _process(delta):
 			print("Mouse Position (global):", mouse_pos)
 		print("Mouse Position (local to tilemap):", local_mouse_pos)
 		print("Tile Map Position:", tile_map_pos)
-
+# first plant implementation with beetrot
 	if Input.is_action_just_pressed("plant"):
 		if tileSource == 3 and tile_map.get_cell_atlas_coords(0, tile_map_pos) == Vector2i(1,0):
 			tile_map.set_cell(0, tile_map_pos, 4, Vector2i(1,0), 0)
+			await get_tree().create_timer(randi_range(30, 180)).timeout
+			tile_map.set_cell(0, tile_map_pos, 1, Vector2i(1,0), 0)
+			
 		elif tileSource == 3 and tile_map.get_cell_atlas_coords(0,tile_map_pos) == Vector2i(0,0):
 			tile_map.set_cell(0, tile_map_pos, 4, Vector2i(0,0), 0)
+			await get_tree().create_timer(randi_range(30, 180)).timeout
+			tile_map.set_cell(0, tile_map_pos, 1, Vector2i(0,0), 0)
+			
 		elif tileSource == 3 and tile_map.get_cell_atlas_coords(0,tile_map_pos) == Vector2i(2,0):
 			tile_map.set_cell(0, tile_map_pos, 4, Vector2i(2,0), 0)
+			await get_tree().create_timer(randi_range(30, 180)).timeout
+			tile_map.set_cell(0, tile_map_pos, 1, Vector2i(2,0), 0)
